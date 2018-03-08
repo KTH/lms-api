05d564559423a8689af0ff1191f68e673a176191
Emil Stenberg <esandin@gmail.com>
2018-03-08 11:14:10 +0100

    added a test

--- test/integration/specs/courses.test.js
+++ test/integration/specs/courses.test.js
@@ -1,16 +1,18 @@
 require('dotenv').config()
-require('should')
 const test = require('tape')
-const sinon = require('sinon')
 const course = require('../../../server/api/course')
 
 // This test takes a while to complete...
 test('should cache course 85, Canvas at KTH', async t => {
   const req = {}
-  const res = {status: sinon.spy(), json: sinon.spy()}
+  let body
+  const res = {status: sinon.spy(), json: _body=>{
+    body = _body
+  })}
   res.json('foo')
+  console.log('body called with:', body)
   /* await */ // course.allCourses(req, res)
-  sinon.assert.calledWith(res.json, 'foo')
+  // sinon.assert.calledWith(res.json, {})
   // res.json.should.have.been.calledWith({1: 2})
   t.end()
 })
