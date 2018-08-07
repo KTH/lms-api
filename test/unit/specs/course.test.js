e398045ed76aae9190dcc37f2c842cb1e5322e11
Emil Stenberg <emilsten@kth.se>
2018-08-07 12:52:49 +0200

    removed unused code

--- test/unit/specs/course.test.js
+++ test/unit/specs/course.test.js
@@ -3,9 +3,7 @@ const rewire = require('rewire')
 const course = rewire('../../../server/api/course')
 const sinon = require('sinon')
 
-test('should convert a Map to a standard JS object', async t => {
-  const res = sinon.spy()
-  const req = sinon.spy()
+test('strMapToObj should convert a Map to a standard JS object', async t => {
   const map = new Map()
   map.set('a course code', {id:123})
   const strMapToObj = course.__get__('strMapToObj')
