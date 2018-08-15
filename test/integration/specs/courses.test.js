e708201eba652344f5869fbfbd374113275158ed
Carlos Saito <c.saito.style@gmail.com>
2018-08-15 15:28:21 +0200

    Use tape instead of assert

--- test/integration/specs/courses.test.js
+++ test/integration/specs/courses.test.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const test = require('tape')
 const course = require('../../../server/api/course')
 const sinon = require('sinon')
-const assert = require('assert')
+
 // This test takes a while to complete...
 test('should cache course 85, Canvas at KTH', async t => {
   const req = {}
@@ -15,7 +15,6 @@ test('should cache course 85, Canvas at KTH', async t => {
   }
 
   await course.allCourses(req, res)
-  assert.ok(body['canvas-at-kth'])
-
+  t.ok(body['canvas-at-kth'])
   t.end()
 })
