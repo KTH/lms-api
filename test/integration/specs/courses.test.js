aa7d9845102ad80a445fb0566b9d1b9ccec4d983
Emil Stenberg <emilsten@kth.se>
2018-08-20 14:36:34 +0200

    fix lint errors

--- test/integration/specs/courses.test.js
+++ test/integration/specs/courses.test.js
@@ -1,20 +1,18 @@
 require('dotenv').config()
 const test = require('tape')
 const course = require('../../../server/api/course')
-const sinon = require('sinon')
 const assert = require('assert')
 // This test takes a while to complete...
 test('should cache course 85, Canvas at KTH', async t => {
   const req = {}
   const resMock = {
-    status: ()=>{},
+    status: () => {},
     json (body) {
-        // Check that the course 'canvas-at-kth' is cached alright 
-        assert.ok(body['canvas-at-kth'])
-        t.end()
+      // Check that the course 'canvas-at-kth' is cached alright
+      assert.ok(body['canvas-at-kth'])
+      t.end()
     }
   }
 
   await course.allCourses(req, resMock)
-
 })
