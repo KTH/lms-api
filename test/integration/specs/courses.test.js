08a6620b939ba5af7befc04e6e9f4d8706290c05
Emil Stenberg <emilsten@kth.se>
2018-08-20 14:52:34 +0200

    removed redundant require

--- test/integration/specs/courses.test.js
+++ test/integration/specs/courses.test.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 const test = require('tape')
 const course = require('../../../server/api/course')
-const assert = require('assert')
-// This test takes a while to complete...
+
 test('should cache course 85, Canvas at KTH', async t => {
   const req = {}
   const resMock = {
     status: () => {},
     json (body) {
       // Check that the course 'canvas-at-kth' is cached alright
-      assert.ok(body['canvas-at-kth'])
+	t.ok(body['canvas-at-kth'])
       t.end()
     }
   }
