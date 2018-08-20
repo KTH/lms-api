0ae6cec3a971cafb7e01a32be506be485c8ca007
Emil Stenberg <emilsten@kth.se>
2018-08-20 14:55:22 +0200

    fix lint errors

--- test/integration/specs/courses.test.js
+++ test/integration/specs/courses.test.js
@@ -8,7 +8,7 @@ test('should cache course 85, Canvas at KTH', async t => {
     status: () => {},
     json (body) {
       // Check that the course 'canvas-at-kth' is cached alright
-	t.ok(body['canvas-at-kth'])
+      t.ok(body['canvas-at-kth'])
       t.end()
     }
   }
