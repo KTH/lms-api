f7c95e4b44d95a1270723c9eb819fae611ab5d50
Carlos Saito <c.saito.style@gmail.com>
2018-08-27 15:37:49 +0200

    Merge "api/course" to "api/index"

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,49 @@
 
 const express = require('express')
 const router = express.Router()
-const course = require('./course')
+const simpleCache = require('../../simpleCache')
+const log = require('../../logger')
 
-router.get('/courses', course.allCourses)
-router.get('/courses/:courseCode', course.course)
+function strMapToObj (strMap) {
+  let obj = Object.create(null)
+
+  if (strMap) {
+    for (let [k, v] of strMap) {
+      obj[k] = v
+    }
+  }
+  return obj
+}
+
+async function getCourse (req, res) {
+  try {
+    const coursesMap = await simpleCache.courses
+    const courses = []
+    for (let [, value] of coursesMap) {
+      courses.push(value)
+    }
+    const result = courses.filter(course => course.course_code === req.params.courseCode)
+    if (result.length === 0) {
+      res.sendStatus(404)
+    } else {
+      res.json(result)
+    }
+  } catch (e) {
+    log.error('An error occured:', e)
+    res.status(500).send('Internal server error!')
+  }
+}
+
+async function getAllCourses (req, res) {
+  try {
+    const coursesMap = await simpleCache.courses
+    res.json(strMapToObj(coursesMap))
+  } catch (e) {
+    log.error('An error occured:', e)
+    res.status(500).send('Internal server error!')
+  }
+}
+
+router.get('/courses', getAllCourses)
+router.get('/courses/:courseCode', getCourse)
 module.exports = router
--- test/unit/specs/course.test.js
+++ test/unit/specs/course.test.js
@@ -1,12 +1,12 @@
 const test = require('tape')
 const rewire = require('rewire')
-const course = rewire('../../../server/api/course')
+const api = rewire('../../../server/api')
 const sinon = require('sinon')
 
 test('the function strMapToObj should convert a Map to a standard JS object', async t => {
   const map = new Map()
   map.set('a course code', {id: 123})
-  const strMapToObj = course.__get__('strMapToObj')
+  const strMapToObj = api.__get__('strMapToObj')
   const result = await strMapToObj(map)
 
   t.deepEqual(result, {
@@ -16,7 +16,7 @@ test('the function strMapToObj should convert a Map to a standard JS object', as
 })
 
 test('the function strMapToObj should return an empty object if the map is null', async t => {
-  const strMapToObj = course.__get__('strMapToObj')
+  const strMapToObj = api.__get__('strMapToObj')
   const result = await strMapToObj(null)
 
   t.deepEqual(result, {})
@@ -26,13 +26,14 @@ test('the function strMapToObj should return an empty object if the map is null'
 // TODO: replace this test with an integration test without mocks/stubs
 test('the function allCourses should render the cached courses', async t => {
   const res = {json: sinon.stub()}
+  const getAllCourses = api.__get__('getAllCourses')
 
   // Mock the calls to Canvas, we have one cached course
-  course.__set__('simpleCache', {courses: Promise.resolve(new Map([
+  api.__set__('simpleCache', {courses: Promise.resolve(new Map([
     ['SF1624', {}]
   ]))})
 
-  await course.allCourses(null, res)
+  await getAllCourses(null, res)
 
   t.ok(res.json.calledWith({'SF1624': {}}))
   t.end()
