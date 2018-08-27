fbbf723a6d1ff7d765574b76d005759f4551cdd2
Carlos Saito <c.saito.style@gmail.com>
2018-08-27 15:31:56 +0200

    Remove unused code

--- server/server.js
+++ server/server.js
@@ -1,18 +1,7 @@
-'use strict'
 const server = require('kth-node-server')
 const api = require('./api')
 const systemRoutes = require('./systemroutes')
 const log = require('../logger')
-// Load .env file in development mode
-const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase()
-if (nodeEnv === 'development' || nodeEnv === 'dev' || !nodeEnv) {
-  require('dotenv').config()
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require('../config/localSettings')
-}
 
 server.use('/api/lms-api/', systemRoutes)
 server.use('/api/lms-api/api/', api)
