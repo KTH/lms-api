ab4ece4af0b1e3529ecf544db824da8fb8789c03
Carlos Saito <c.saito.style@gmail.com>
2018-08-27 15:30:26 +0200

    Remove unused code

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-'use strict'
-// Load .env file in development mode
-const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase()
+require('dotenv').config()
 const logger = require('./logger')
-if (nodeEnv === 'development' || nodeEnv === 'dev' || !nodeEnv) {
-  require('dotenv').config()
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require('./config/localSettings')
-}
-
 const config = require('./config/serverSettings')
-require('./simpleCache').start()
 
+require('./simpleCache').start()
 const server = require('./server/server')
 
 module.exports = server.start({
